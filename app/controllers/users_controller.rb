33c9726001d15a6fa4f6b071d7bed106c911d3b3
sensei100 <jenanton01@gmail.com>
2016-06-16 15:29:51 +0300

    fixed signup and got login working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,27 @@ class UsersController < ApplicationController
     if params[:password] == ""
       redirect '/signup'
     end
-    user = User.new(params)
+    user = User.create(username: params[:username], email: params[:email], password: params[:password])
     if user.username != "" && user.email != ""
       user.save
       session[:id] = user.id
       redirect to "/books"
     else
       redirect to '/signup'
+    end
+  end
+
+  get '/login' do
+      erb :login
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/books"
+    else
+      erb :login
     end
   end
 
