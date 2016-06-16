70afba1fb3c0f242d9aa15cdbe5ae951089c483d
sensei100 <jenanton01@gmail.com>
2016-06-16 16:37:23 +0300

    started working on student conroller and adding new students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,15 @@
 class StudentsController < ApplicationController
 
+  get '/students' do
+    erb :'/students/index'
+  end
+
+  get '/students/new' do
+    erb :'/students/new'
+  end
+
+  post '/students' do
+    erb :'/students/index'
+  end
+
 end
