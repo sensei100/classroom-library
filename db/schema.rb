6889c7c31c16d9d9243e2405b72c93219cd01365
sensei100 <jenanton01@gmail.com>
2016-06-21 10:58:05 -0500

    added user_id column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621155126) do
+ActiveRecord::Schema.define(version: 20160621155602) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
     t.string  "author"
     t.string  "genre"
     t.integer "student_id"
+    t.integer "user_id"
   end
 
   create_table "loans", force: :cascade do |t|
