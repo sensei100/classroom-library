cf8e2a35c73cf1a3a46be9b32bfde9beda9c7577
sensei100 <jenanton01@gmail.com>
2016-06-21 11:41:52 -0500

    added helper methods to books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,18 @@
 class BooksController < ApplicationController
 
   get '/books/new' do
+    if !logged_in?
+      redirect to '/login'
+    else
     erb :'/books/new'
+    end
   end
 
   get '/books/index' do
+    if !logged_in?
+      redirect to '/login'
+    else
     erb :'books/index'
+    end
   end
 end
