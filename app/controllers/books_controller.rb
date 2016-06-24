95b1e6424a6cc7bbe153d4d278f7beb7f0dbd57f
sensei100 <jenanton01@gmail.com>
2016-06-24 16:11:32 -0400

    working on book index view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,35 @@
 class BooksController < ApplicationController
 
+  get '/books' do
+   if !logged_in?
+      redirect to '/login'
+    else
+      @books = Book.all
+      erb :'books/index'
+    end
+  end
+
   get '/books/new' do
     if !logged_in?
       redirect to '/login'
     else
-    erb :'/books/new'
-    end
-  end
-
-  get '/books' do
-    if !logged_in?
-      redirect to '/login'
-    else
-    erb :'books/index'
+      erb :'/books/new'
     end
   end
 
   post '/books' do
-    @book = Book.create(:name => params[:book])
-    @book.save
+    @book = Book.create(name: params[:name], author: params[:author], genre: params[:genre])
     redirect "/books/#{@book.slug}"
   end
 
   get '/books/:slug' do
     @book = Book.find_by_slug(params[:slug])
     erb :'books/show'
+  end
+
+  post '/books/:slug' do
+    @book = Book.find_by_slug(params[:slug])
+    @book.update(params[:book])
   end
 
 end
