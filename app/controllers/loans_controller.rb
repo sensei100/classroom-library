c7f873d17877ed8c4f89609fc9ac1de20e134a87
sensei100 <jenanton01@gmail.com>
2016-06-27 11:02:57 -0400

    began working on loans

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -3,4 +3,9 @@ class LoansController < ApplicationController
   get '/loans' do
     erb :'/loans/new'
   end
+
+  post '/loans' do
+    @loan = Loan.create(params[:loan])
+    redirect to "/books/index"
+  end
 end
