4ba9c22066efde64d1183f47d28cfd092b8993e4
sensei100 <jenanton01@gmail.com>
2016-07-07 08:32:06 -0500

    user sees only their books and students in loans view

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,16 +1,22 @@
 class LoansController < ApplicationController
 
   get '/loans' do
+    if !logged_in?
+      redirect to '/login'
+    else
+      @loans = current_user.loans
+      @books = current_user.books
+      @students = current_user.students
     erb :'/loans/new'
+    end
   end
 
   post '/loans' do
     @user = current_user
     @book = Book.find_by_slug(params[:slug])
-    @loan = Loan.create(student_id: params[:student_id], book_id: params[:book_id])
+    @loan = @user.loan.create(student_id: params[:student_id], book_id: params[:book_id])
     @book.update(book: params[:book])
     @lendee = params[:student_id]
-    @book = Book.find_by_slug(params[:slug])
     @book.checked_out = lendee
     @book.save
     redirect to '/books/index'
