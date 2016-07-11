82026dc0d8781bac41e87bac38547f353182b172
sensei100 <jenanton01@gmail.com>
2016-07-11 14:46:41 -0500

    working on deleting loans

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -2,7 +2,8 @@ class LoansController < ApplicationController
 
     get '/loans' do
       @loans = current_user.loans
-      @book = Book.find_by_slug(params[:slug])
+      @books = current_user.books
+      @students = current_user.students
       if logged_in?
         erb :'loans/index'
       else
@@ -21,41 +22,36 @@ class LoansController < ApplicationController
     end
   end
 
-
-
   post '/loans' do
     @user = current_user
     @book = Book.find_by(id: params[:book_id])
-    @student = Student.find_by_name(params[:name])
+    @student = Student.find_by_name(params[:student_id])
     @loan = @user.loans.create(name: @book.name, student_id: params[:student_id], book_id: params[:book_id])
     @loan.save
-    redirect to "/loans"
+    redirect to "/loans/:slug"
      
   end
 
   get '/loans/:slug' do
     @loans = current_user.loans
-    @books = current_user.books
+    @student = Student.find_by_name(params[:name])
+    @books = Book.find_by_slug(params[:slug])
+
     erb :'/loans/show'
   end
 
   post '/loans/:slug' do
     @user = current_user
-    @loan = Loan.find_by_name(name: @book.name)
-    @book.author = params[:author]
-    @book.genre = params[:genre]
-    @book.user_id = params[:user_id]
-    @student = @current_user.student
+    @loan = Loan.find_by_name(name: @books.name)
+    @book = Book.find_by_slug(params[:slug])
+    @student = Student.find_by_name(name: @student.name)
     @loan.save
   end
 
-  patch '/loans/return' do
-    redirect to "/books/#{@book.slug}"
-  end
-
   delete '/loans/:slug/delete' do
-    @book = Book.find_by_slug(params[:slug])
-    @book.delete
+    @loan = current_user.loans
+    @books = Book.find_by_slug(params[:slug])
+    @loan.delete
     redirect to '/loans'
   end
   
