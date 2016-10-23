0fa2e6932f666707c0e1dc19ba2f3cf35c7171a6
sensei100 <jenanton01@gmail.com>
2016-10-23 16:12:52 -0400

    updated config.ru

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,28 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-if :development?
-  set :database, {
-      adapter: "sqlite3",
-      database: "db/db.sqlite3"
-  }
-else
-   set :database, ENV['SINATRA_ENV']
+configure :development do
+# set :database, 'sqlite:///dev.db'
+set :database, 'sqlite3:database.db'
+set :show_exceptions, true
 end
-require 'rack-flash'
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
 require_all 'app'
