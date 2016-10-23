b7cfc15fafdc001c5180acb9f416cd767987969f
sensei100 <jenanton01@gmail.com>
2016-10-23 09:11:28 -0400

    updated config.ru

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-if development?
+if :development?
   set :database, {
       adapter: "sqlite3",
       database: "db/db.sqlite3"
